41ec3989699bce3f34546befa9f4a873e48e5c87
Chanpreet Singh <88098292+Chanpreet02@users.noreply.github.com>
2024-09-09 09:57:50 +0530

    Create Arithmatic_operators.kt

---
+++ Arithmatic_operators.kt
@@ -0,0 +1,18 @@
+/*
+Arithematic Operators
+(+)	  Addition	||  Adds together two values	||  x + y
+(-)	  Subtraction  ||	Subtracts one value from another  ||	x - y
+(*)	  Multiplication  ||	Multiplies two values  ||	x * y
+(/)	  Division  ||	Divides one value by another  ||	x / y
+(%)	  Modulus  ||	Returns the division remainder  ||	x % y
+*/
+fun main() {
+   val x: Int = 40
+   val y: Int = 20
+
+   println("x + y = " +  (x + y)) 
+   println("x - y = " +  (x - y))
+   println("x / y = " +  (x / y))
+   println("x * y = " +  (x * y))
+   println("x % y = " +  (x % y))
+}
