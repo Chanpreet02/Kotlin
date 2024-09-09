4bb710d1b76ece3235711b98be9c7dd681199b68
Chanpreet Singh <88098292+Chanpreet02@users.noreply.github.com>
2024-09-09 10:04:20 +0530

    Create Relational_operators.kt

---
+++ Relational_operators.kt
@@ -0,0 +1,21 @@
+/*
+(>)  ||	greater than  ||	x > y
+(<)  ||	less than  ||	x < y
+(>=)  ||	greater than or equal to  ||	x >= y
+(<=)  ||	less than or equal to	  ||  x <= y
+(==)  ||	is equal to	  ||  x == y
+(!=)  ||	not equal to  ||	x != y
+*/
+
+fun main(){
+  val x: Int = 50
+   val y: Int = 25
+
+   println("x > y = " +  (x > y))
+   println("x < y = " +  (x < y))
+   println("x >= y = " +  (x >= y))
+   println("x <= y = " +  (x <= y))
+   println("x == y = " +  (x == y))
+   println("x != y = " +  (x != y))
+
+}
