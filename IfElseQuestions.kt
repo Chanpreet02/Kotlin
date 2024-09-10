e2ea09fab8bea9347d96b0b8ea624b7d58d3c756
Chanpreet Singh <88098292+Chanpreet02@users.noreply.github.com>
2024-09-10 14:29:08 +0530

    Questions for If else condition
    Practice questions for If else condition in Kotlin

---
+++ IfElseQuestions.kt
@@ -0,0 +1,18 @@
+fun main(){
+  // Write a Kotlin program to check whether a number is positive or negative
+  val number = 45
+  if(number>0){
+    println("Positive")
+  }
+  else{
+    println("Negative)
+  }
+  
+            
+}
+
+/*
+Output
+Positive
+
+*/
