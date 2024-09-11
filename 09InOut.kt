7a75cb24ab2ad1747586b436be344d8c113a9c55
Chanpreet Singh <88098292+Chanpreet02@users.noreply.github.com>
2024-09-11 12:48:39 +0530

    InOut.kt
    Some more updates in order to take input from the user

--- 09InOut.kt
+++ 09InOut.kt
@@ -7,5 +7,20 @@ fun main(){
     val age:Int = readLine()?.toIntOrNull() ?:0
     println("Hello $nameKotlin vai tu $age saal ka hai")
 
+
+    println("Please Enter any String value: ")
+    var sTRing=readLine()
+    println("Please Enter any Integer value: ")
+    var INt= readLine()?.toIntOrNull()?:0
+    println("Please Enter any Character value: ")
+    var ch = readLine()?.firstOrNull()?: ' '
+    println("Please Enter any Decimal value: ")
+    var num = readLine()?.toDoubleOrNull()?:0.0
+    println("Thank You for entering, here is the result: ")
+    println("String Value: $sTRing")
+    println("Integer Value: $INt")
+    println("Character Value: $ch")
+    println("Decimal Value: $num")
+
 }
 
