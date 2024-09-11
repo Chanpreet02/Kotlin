4fae1df285c307b3805787b2e8b356b4247ee89a
Chanpreet Singh <88098292+Chanpreet02@users.noreply.github.com>
2024-09-11 12:51:08 +0530

    BMI.kt
    Added information about the BMI which tells whether the person is under weight, fine, over weight or an obese.

--- 10BMI.kt
+++ 10BMI.kt
@@ -19,5 +19,19 @@ fun main() {
     }
     val BMI: Double = weight / (height * height)
     println("Your BMI is: $BMI")
+
+    if (BMI <= 18.5) {
+        println("You are underweight please focus on eating more")
+    } else if (BMI > 18.5 && BMI < 24.9) {
+        println("You are doing just fine")
+    } else if (BMI >= 24.9 && BMI < 29.9) {
+        println("You are just a bit overweight. Eat Less")
+    } else if (BMI >= 29.9 && BMI < 34.9) {
+        println("You are on obese level 1 Bro Focus more on your body please")
+    } else if (BMI >= 34.9 && BMI < 39.9) {
+        println("You are on obese level 2. Please workout more and eat very less")
+    } else if (BMI >= 39.9) {
+        println("You are gonna die soon go and have some operation")
+    }
     
 }
