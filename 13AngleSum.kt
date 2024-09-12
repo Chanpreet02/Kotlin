258df27b8c16fa967da6eaefce10b604f8a50fa8
Chanpreet Singh <88098292+Chanpreet02@users.noreply.github.com>
2024-09-12 15:41:44 +0530

    AngleSum.kt
    Kotlin program to check whether the three angles are of a perfect triangle or not. In this update I added the functionality to take input from the user as well as re run the program

--- 13AngleSum.kt
+++ 13AngleSum.kt
@@ -3,16 +3,30 @@ Kotlin program to check whether the three angles are of a perfect triangle or no
 */
 
 fun main(){
-  var angle1:Double = 60.0
-  var angle2:Double = 45.0
-  var angle3:Double = 75.0
-  var sum:Double = 0.0
+  sumAngle()
+}
 
-  sum = angle1+angle2+angle3
-  if(sum==180.0){
-    println("These are the angles of a perfect triangle")
-  }
-  else{
-    println("These angles does not belong to a perfect triangle")
-  }
+fun sumAngle(){
+    println("Enter the three angles of a triangle and check whether the angles are of a valid triangle or not")
+    var angle1:Double = readLine()?.toDouble()?:0.0
+    var angle2:Double = readLine()?.toDouble()?:0.0
+    var angle3:Double = readLine()?.toDouble()?:0.0
+
+    var sum:Double = 0.0
+    sum = angle1+angle2+angle3
+    if(sum==180.0){
+        println("These are the angles of a proper triangle")
+    }
+    else {
+        println("These are not the angles of a perfect triangle")
+    }
+
+    println("Do you want to rerun the program and enter some different angles?")
+    val result = readLine()?.lowercase()
+    if(result == "yes" || result=="y"){
+        sumAngle()
+    }
+    else{
+        println("Thank You")
+    }
 }
