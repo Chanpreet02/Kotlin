cca41db9dec6d3765a6d26ed9efebd7e71b50ea3
Chanpreet Singh <88098292+Chanpreet02@users.noreply.github.com>
2024-09-13 12:48:45 +0530

    While.kt
    Kotlin Program to practice some loops on while statement

--- 14While.kt
+++ 14While.kt
@@ -6,7 +6,7 @@ fun main(){
 Pattern()
 SumEvenNumbers()
 SumEvenNumbersN()
-
+Factorial()
 }
 
 
@@ -50,4 +50,18 @@ fun SumEvenNumbersN(){
         n++
     }
     println("The sum of first n even numbers is: $sum")
+}
+
+
+
+//Function to print the factorial of a given number
+fun Factorial(){
+    var num:Int = readLine()?.toInt()?:0
+    var n=num
+    var fact =1
+    while(n>0){
+        fact=fact*n
+        n--
+    }
+    println("Factorial of $num is: $fact")
 }
