021b3ffb58c492197683a77fa933701b178a8cbb
Chanpreet Singh <88098292+Chanpreet02@users.noreply.github.com>
2024-09-17 15:37:55 +0530

    For.kt
    Basic Kotlin program, along with Syntax to understand the concepts of for loop

---
+++ 15For.kt
@@ -0,0 +1,18 @@
+/*
+Kotlin program to understand the concepts of for loop
+Syntax:
+
+    for (item in collection) {
+        // body of loop
+    }
+*/
+
+fun main(){
+  basic()
+}
+
+fun basic(){
+   for (item in 1..5) {
+      println(item)
+   }
+}
