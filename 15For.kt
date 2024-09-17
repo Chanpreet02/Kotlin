4c8730ea8a3a9f6009925d4ba9e5e6eb0504f064
Chanpreet Singh <88098292+Chanpreet02@users.noreply.github.com>
2024-09-17 15:44:02 +0530

    For.kt
    Kotlin function to decrement the numbers present with the gap of 2

--- 15For.kt
+++ 15For.kt
@@ -9,10 +9,21 @@ Syntax:
 
 fun main(){
   basic()
+  decrementByTwo()
 }
 
 fun basic(){
    for (item in 1..5) {
       println(item)
    }
+}
+
+fun decrementByTwo(){//This is a function to decrement the numbers present with the gap of 2
+        for (item in 5 downTo 1 step 2) {
+        println(item) 
+        /* Output: 5
+                   3
+                   1
+        */
+    }
 }
