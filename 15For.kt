bbeeaba70f562809489f2d5ecb287e066940f4e4
Chanpreet Singh <88098292+Chanpreet02@users.noreply.github.com>
2024-09-17 15:52:11 +0530

    For.kt
    Function to print the multiples of 5 from 1 to 50

--- 15For.kt
+++ 15For.kt
@@ -11,17 +11,18 @@ fun main(){
   basic()
   decrementByTwo()
   Even()
+  Table()
 }
 
 fun basic(){
-   for (item in 1..5) {
-      println(item)
+   for (i in 1..5) {
+      println(i)
    }
 }
 
 fun decrementByTwo(){//This is a function to decrement the numbers present with the gap of 2
-        for (item in 5 downTo 1 step 2) {
-        println(item) 
+        for (i in 5 downTo 1 step 2) {
+        println(i) 
         /* Output: 5
                    3
                    1
@@ -34,3 +35,14 @@ fun Even(){ //Function to print even numbers from 2 till 20
         println(i)
     }
 }
+
+
+fun Table(){//Function to print the multiples of 5 from 1 to 50
+    val number = 5
+    for (i in 1..10) {
+        println("$number x $i = ${number * i}")
+    }
+}
+
+
+
