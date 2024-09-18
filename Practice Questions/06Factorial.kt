e331d13b900e41e486f03688b036e97ea85503f4
Chanpreet Singh <88098292+Chanpreet02@users.noreply.github.com>
2024-09-18 12:45:40 +0530

    Factorial.kt
    Recursive program to find the factorial of a given number

--- Practice Questions/06Factorial.kt
+++ Practice Questions/06Factorial.kt
@@ -12,7 +12,18 @@ fun Factorial(){
     println("Factorial of $num is: $fact")
 }
 
+//Recursive program for finding the factorial
+fun Fact(num:Int):Int{
+    if(num>0){
+    return num*Fact(num-1)
+    }
+    else{
+        return 1
+    }
+}
+
 
 fun main(){
   Factorial()
+  println(Fact(5))//Output = 120
 }
